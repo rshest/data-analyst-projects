466ab38261555abd16ed53b8e3991aa1f207aaa8
Ruslan Shestopalyuk <rushbits@gmail.com>
2017-05-28 00:05:16 +0200

    feat: Command line arguments for the scraper

--- p4.explore-and-summarize-data/scrape_data.py
+++ p4.explore-and-summarize-data/scrape_data.py
@@ -1,11 +1,16 @@
+import argparse
 import urllib
 import csv
 import time
 
 from bs4 import BeautifulSoup
 
+
 SITE = 'http://www.vgchartz.com/gamedb/'
 RESULTS_PER_PAGE = 1000
+MAIN_TABLE_CLASS = 'chart'
+OUT_FILE = 'vgtitles.csv'
+TITLE = 'Web-scraper for {}'.format(SITE)
 
 
 def preprocess(str):
@@ -19,7 +24,7 @@ def preprocess(str):
 def parse_page(data, parse_header=False):
 	bs = BeautifulSoup(data, 'lxml')
 	
-	table = bs.find('table', class_='chart')
+	table = bs.find('table', class_=MAIN_TABLE_CLASS)
 	rows = table.find_all('tr')
 
 	def get_cols(row, tag):
@@ -39,12 +44,12 @@ def parse_page(data, parse_header=False):
 			pass
 
 
-def scrap():
+def scrape(out_file, start_page=1, sleep_seconds=1):
 	params = {'results': RESULTS_PER_PAGE}
 
-	cur_page = 1
+	cur_page = start_page
 
-	f = open('vgtitles.csv', 'wb')
+	f = open(out_file, 'ab')
 	csv_writer = csv.writer(f)
 
 	while True:	
@@ -53,8 +58,7 @@ def scrap():
 
 		print 'Parsing page {}: {}'.format(cur_page, url)
 
-		data = urllib.urlopen(url).read()
-		
+		data = urllib.urlopen(url).read()		
 		rows = list(parse_page(data, cur_page == 1))
 
 		if len(rows) == 0:
@@ -66,9 +70,19 @@ def scrap():
 		cur_page += 1
 		
 		f.flush()
-		time.sleep(1)
+		time.sleep(sleep_seconds)
 
 	f.close()
 
+
 if __name__ == '__main__':
-	scrap()
+	parser = argparse.ArgumentParser(description=TITLE)
+	parser.add_argument('-s', '--start-page', dest='start_page', required=False, type=int, default=1,
+                    	help='number of the starting page to scrape')
+	parser.add_argument('-f', '--file', dest='file', required=False, default=OUT_FILE,
+                    	help='output CSV file')
+	parser.add_argument('-t', '--sleep', dest='sleep', required=False, type=int, default=1,
+                    	help='period to sleep between pages')
+	params = parser.parse_args()
+
+	scrape(params.file, params.start_page, params.sleep)
